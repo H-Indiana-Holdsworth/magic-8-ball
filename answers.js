2eb5167d5d93ba8d77fd0af79ca425ec5cb61939
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-10-05 14:55:52 -0700

    Adds button and displays 8 ball answer

---
+++ answers.js
@@ -0,0 +1,11 @@
+export const answers = [
+    `No chance homie`,
+    `Tough luck`,
+    `Fo sho`,
+    `Absitively posolutely`,
+    `Yes a thousand times`,
+    `Big no`,
+    `Honestly, my sources are just mumbling, I can't understand a word they're saying`,
+    `You should ask that to a magic 7 ball, you know, I'm actually afraid of 7 because 7 8 9`,
+    `Why are you asking questions to a piece of code someone wrote`,
+];
--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 // import functions and grab DOM elements
+import { answers } from './answers.js';
 
+const button = document.getElementById('button');
+const answerSpan = document.getElementById('answer-span');
 // initialize global state
 
 // set event listeners 
+const getRandomNumber = (max)=>{
+    return Math.floor(Math.random() * max);
+};
+
+button.addEventListener('click', () => {
+
+    const randomIndex = getRandomNumber(answers.length);
+
+    answerSpan.textContent = answers[randomIndex];
+
+});
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
